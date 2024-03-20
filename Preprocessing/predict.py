a2f2329011f75908099f73f4b05c1ae005f46580
bme3412 <54747855+bme3412@users.noreply.github.com>
2024-03-19 20:02:54 -0400

    Add files via upload

---
+++ Preprocessing/predict.py
@@ -0,0 +1,67 @@
+"""Here's my prediction for the bracket based on the given probabilities and using rational logic to account for potential upsets:
+
+First Round:
+- Virginia defeats Grambling 
+- UConn defeats Montana St.
+- San Diego St. defeats Colorado St. 
+- Duke defeats UAB
+- Yale upsets Auburn
+- Illinois defeats BYU
+- Baylor defeats Duquesne
+- Arizona defeats Morehead St.
+- Drake upsets Washington St. 
+- Iowa St. defeats South Dakota St.
+- North Carolina defeats Howard/Wagner
+- Michigan St. defeats Mississippi St.
+- Saint Mary's defeats Grand Canyon
+- Boise St./Colorado defeats Alabama
+- Charleston upsets Dayton 
+- Texas Tech defeats Nevada
+
+Second Round: 
+- UConn defeats Virginia
+- San Diego St. defeats Duke
+- Illinois defeats Yale  
+- Arizona defeats Baylor
+- Iowa St. defeats Drake
+- North Carolina defeats Michigan St.
+- Saint Mary's defeats Boise St./Colorado
+- Texas Tech defeats Charleston
+
+Sweet 16:
+- UConn defeats San Diego St.
+- Illinois defeats Arizona
+- North Carolina defeats Iowa St.
+- Saint Mary's defeats Texas Tech
+
+Elite 8:
+- UConn defeats Illinois
+- Saint Mary's defeats North Carolina
+
+Final Four:
+- Saint Mary's defeats UConn
+
+Other side of the bracket:
+- Purdue defeats Texas
+- Tennessee defeats Saint Peter's
+- Gonzaga defeats Samford 
+- South Carolina defeats Oregon
+- Marquette defeats Vermont
+- Kentucky defeats Akron
+- Kansas defeats McNeese
+- Creighton defeats Virginia/Colorado St.
+
+- Purdue defeats Tennessee 
+- Gonzaga defeats South Carolina
+- Kentucky defeats Marquette
+- Kansas defeats Creighton
+
+- Purdue defeats Gonzaga
+- Kentucky defeats Kansas
+
+- Purdue defeats Kentucky
+
+Predicted Championship:
+Saint Mary's defeats Purdue
+
+This factors in the given probabilities heavily, while also predicting a few upsets based on matchups and tournament trends. Of course, the actual results are always unpredictable!"""
